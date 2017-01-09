944ce9d9661333c2e2b2c07bbf713c29b23d43b9
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2017-01-09 11:20:55 +0700

    Update cucumber-jvm version to 1.2.5 which fixes timing issues Add start cucumber options directed to junit

--- src/test/java/ru/yandex/qatools/allure/cucumberjvm/Steps.java
+++ src/test/java/ru/yandex/qatools/allure/cucumberjvm/Steps.java
@@ -3,7 +3,6 @@ package ru.yandex.qatools.allure.cucumberjvm;
 import org.junit.Assert;
 
 import ru.yandex.qatools.allure.annotations.Attachment;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
