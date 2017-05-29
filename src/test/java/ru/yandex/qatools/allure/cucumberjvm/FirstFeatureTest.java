38f7cb3a7ac62ec90f5d42a37c9aa33b0cfda800
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2017-05-29 13:30:55 +0700

    Add data table attachments

--- src/test/java/ru/yandex/qatools/allure/cucumberjvm/FirstFeatureTest.java
+++ src/test/java/ru/yandex/qatools/allure/cucumberjvm/FirstFeatureTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/"}, tags = "@first")
+@CucumberOptions(features = {"src/test/resources/"}, tags = "@table")
 public class FirstFeatureTest {
     
 }
